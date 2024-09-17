53d1074470e792d648003be62d7c2efb499d56fd
Kishore4123 <76401205+Kishore4123@users.noreply.github.com>
2024-09-18 01:52:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+alert("ALL RIGHTS OF THIS WEBSITE IS OWNED BY KISHORE JOHN");
+
+document.getElementById("Model").addEventListener("click",handleModel);
+document.getElementById("Survey").addEventListener("click",handleSurvey);
+
+
+function handleModel(){
+    
+    window.location.href = "https://www.tinkercad.com/things/lvxakoqIGjY-fantastic-elzing-jaiks/edit";
+
+}
+
+function handleSurvey(){
+
+    window.location.href = "./survey.html";
+    
+}
